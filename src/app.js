4ac1cdcab2236fcbd215b8aaf3b80dad53ea3528
alvinxrww <alvin.xavier@ui.ac.id>
2025-01-26 14:15:34 +0700

    feat: register, login, and jwt

---
+++ src/app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./config/database");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 4000;
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log(`Auth Service running on port ${PORT}`));
+});
---
+++ src/controllers/authController.js
@@ -0,0 +1,35 @@
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+    const { name, email, password, role } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+        const user = await User.create({ name, email, password: hashedPassword, role });
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ where: { email } });
+        if (!user) return res.status(404).json({ error: 'User not found' });
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) return res.status(401).json({ error: 'Invalid password' });
+
+        const token = jwt.sign(
+            { id: user.id, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+
+        res.json({ token });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
