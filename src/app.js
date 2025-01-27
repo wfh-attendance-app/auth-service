b8b0103d63e84b5b621637cb33872bd270153473
alvinxrww <alvin.xavier@ui.ac.id>
2025-01-27 21:17:40 +0700

    fix: add cors

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const sequelize = require("./config/database");
 const authRoutes = require("./routes/authRoutes");
-
 const app = express();
+const cors = require('cors');
+
+app.use(cors())
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-
 const PORT = process.env.PORT || 4000;
-
 sequelize.sync().then(() => {
   app.listen(PORT, () => console.log(`Auth Service running on port ${PORT}`));
 });
