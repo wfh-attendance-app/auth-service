08ce9c8924ba1918db0a864dae2cc8585f4435a8
alvinxrww <alvin.xavier@ui.ac.id>
2025-01-28 09:41:13 +0700

    feat: get current user

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const { authenticate } = require("../middlewares/authMiddleware");
 const { register, login } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/profile", authenticate, (req, res) => {
+    res.json(req.user); // Current user's data is available in req.user
+});
 
 module.exports = router;
