3d7f5eb67d8bc042b7f4a2afbee1e35520b99c4c
alvinxrww <alvin.xavier@ui.ac.id>
2025-04-13 12:04:53 +0700

    fix: local deployment with postgres

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: 'postgres',
 });
 
 module.exports = sequelize;
